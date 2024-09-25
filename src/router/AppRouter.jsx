6ceb252d4f2c89932b6a5491ec43202a74c3404f
Javier Gonzalez <javgon2306@gmail.com>
2024-09-25 14:43:34 -0300

    add styles

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { LoginPage } from '../auth/pages/LoginPage';
 import { CalendarPage } from '../calendar/pages/CalendarPage';
 
 export const AppRouter = () => {
-    // const authStatus = 'not-authenticated';
+    const authStatus = 'not-authenticated';
 
   return (
    <Routes>
