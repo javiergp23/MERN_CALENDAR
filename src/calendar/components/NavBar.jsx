96bc03625a1d2c87c9b40303ca6dd59065dc36c1
Javier Gonzalez <javgon2306@gmail.com>
2024-09-25 16:10:35 -0300

    react-big-calendar

--- src/calendar/components/NavBar.jsx
+++ src/calendar/components/NavBar.jsx
@@ -1,9 +1,17 @@
 
 export const NavBar = () => {
   return(
-    <>
-        <p>Hola desde Navbar</p>
-    </>
+    <div className="navbar navbar-dark bg-dark mb-4 px-4">
+        <span className="navbar-brand">
+            <i className="fas fa-calendar-alt"></i>
+            &nbsp;
+            Usuario
+        </span>
+        <button className="btn btn-outline-danger">
+            <i className="fas fa-sign-out-alt"></i>
+            &nbsp;<span>Log Out</span>
+        </button>
+    </div>
   )
 
 }
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,47 @@
-import { NavBar } from '../';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import enUs from 'date-fns/locale/en-US'
+
+import { NavBar } from '../'
+import {addHours, format, parse, startOfWeek, getDay} from 'date-fns';
+
+const locales = {
+  'en-US': enUs,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'Cuempleaños del jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours( new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Javier',
+  }
+}]
+
 
  export const CalendarPage = () => {
   return (
     <>
       <NavBar/>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+    />
     </>
   )
 }
